a2b814655cae11bde2b49760effef3687a79a524
PC Home <tuanminphan45@gmai.com>
2025-01-09 14:43:54 +0000

    do somethings

--- TP2.py
+++ TP2.py
@@ -1,1 +1,34 @@
 #init ex2
+class Vertex():
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f'Vertex({self.name})'
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return isinstance(other, Vertex) and self.name == other.name
+
+    def __hash__(self):
+        return hash(self.name)
+
+class Edge():
+    def __init__(self, tuple):
+        self.start, self.end = tuple
+
+    def __str__(self):
+        return f'Edge({self.start} -> {self.end})'
+
+    def __repr__(self):
+        return self.__str__()
+
+class Graph():
+    def __init__(self, vertices, edges):
+        self.vertices = vertices
+        self.edges = edges
+
+    def __str__(self):
+        return f'Graph(V, E): V = {self.vertices}, E = {self.edges}'
